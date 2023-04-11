2409f67afc26b510b847690ce7ceeb8980a45821
Yoshiaki Yagi <yoshiaki.yagi@sola-air.com>
2023-04-11 21:52:22 +0900

    弾丸クラスを修正

--- weapon_bullet.py
+++ weapon_bullet.py
@@ -19,24 +19,31 @@ class WeaponBullet():
         self.ship_weapon = ship_weapon
 
         # 弾丸格納用配列の作成
-        self.bullet_n = MAX_EXIST_BULLET
+        self.bullet_n = 0
         self.bullet_x =[0]*MAX_EXIST_BULLET
         self.bullet_y =[0]*MAX_EXIST_BULLET
         self.bullet_flag =[False]*MAX_EXIST_BULLET
         
     def create(self,color:int=(255,255,255)) -> None:
         # 弾丸の図形作成
-        self.sur = pygame.Surface((self.radius*2,self.radius*2))
-        self.sur.set_colorkey((0, 0, 0))
+        self.view_sur = pygame.Surface((self.radius*2,self.radius*2))
+        self.view_sur.set_colorkey((0, 0, 0))
         pygame.draw.circle(self.sur,color,(self.radius, self.radius),radius=self.radius)
+        
+        # 弾丸のヒットボックス作成
+        self.hitbox_sur = pygame.Surface((self.radius*2*0.8,self.radius*2*0.8))
+        self.hitbox_sur.set_colorkey((0,0,0))
+        pygame.draw.rect()
 
     def set(self):
         self.bullet_flag = True
-        # bull_x[bull_n] = px-16
-        # bull_y[bull_n] = py-32
-        bull_n = (bull_n+1)%BULLET_MAX
+        self.bullet_flag[self.bullet_n] = True
+        self.bullet_x[self.bullet_n] = 16
+        self.bullet_y[self.bullet_n] = 32
+        bull_n = (bull_n+1)%MAX_EXIST_BULLET
 
-
+    def move(self):
+        return 0
 
     def move(self):
         return 0
