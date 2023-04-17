c712ae56653d84b163cb7fbdbb6705bf44bf61de
Y.Y <yagi.yoshiaki04@gmail.com>
2023-04-18 02:10:39 +0900

    当たり判定テスト

--- SANDBOX.py
+++ SANDBOX.py
@@ -18,12 +18,17 @@ def main() -> None:
     pygame.display.set_caption('Space Shipy')
     clock = pygame.time.Clock()
     
-    screen = pygame.display.set_mode((500, 500))
+    screen = pygame.display.set_mode((400, 400))
     SCREEN = screen.get_rect()
     
-    rec = pygame.Rect(0,0,100,100)
+    # rec = pygame.Rect(0,0,100,100)
+    rec1 = pygame.Surface((100,100))
+    rec2 = pygame.Surface((50,50))
+    rec3 = pygame.Surface((100,100))
     # pygame.Surface.blit(re)
-    drawrec = pygame.draw.rect(screen,(255,0,0),rec)
+    drawrec1 = pygame.draw.rect(rec1,(255,0,0),(0,0,100,100))
+    drawrec2 = pygame.draw.rect(rec2,(0,255,0),(0,0,50,50))
+    drawrec3 = pygame.draw.rect(rec3,(0,0,255),(0,0,100,100))
 
     fpscounter:int = 0
     set_timer:int = 0
@@ -33,10 +38,22 @@ def main() -> None:
         
         # 画面(screen)をクリア
         screen.fill((0, 0, 0))
-        # screen.blit(screen,(0,0))
-        rec.move_ip(100,100)
+
+        drawrec1.move_ip(1,1)
+        drawrec1.clamp_ip(screen.get_rect())
+        drawrec2.move_ip(5,5)
+        drawrec2.clamp_ip(screen.get_rect())
+        drawrec3.move_ip(3,3)
+        drawrec3.clamp_ip(screen.get_rect())
+        screen.blit(rec1,drawrec1.topleft)
+        screen.blit(rec2,drawrec2.topleft)
+        screen.blit(rec3,drawrec3.topleft)
+        # screen.blit(rec2,(300,300))
+        print(drawrec1.collidelistall([drawrec2,drawrec3]))
         # ゲームに登場する人/物/背景の位置Update
- 
+
+        
+        
         # 画面(screen)の実表示
         pygame.display.update()
 
@@ -47,7 +64,7 @@ def main() -> None:
                 sys.exit()
 
         # 描画スピードの調整（FPS)
-        clock.tick(60)
+        clock.tick(30)
         # print(clock.get_fps())
         
 if __name__ == "__main__":
