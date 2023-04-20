65f6a8b4b0624bafca3302fb594290115b49cd2c
Yoshiaki Yagi <yoshiaki.yagi@sola-air.com>
2023-04-20 22:10:16 +0900

    弾丸クラス修正

--- weapon_bullet.py
+++ weapon_bullet.py
@@ -23,36 +23,45 @@ class WeaponBullet():
         self.is_hitscan = bool(param['is_hitscan'])
         self.ship_weapon = ship_weapon
         self.sight_vector = pygame.math.Vector2(self.ship_weapon.sight_vector[1][0]-self.ship_weapon.sight_vector[0][0],self.ship_weapon.sight_vector[1][1]-self.ship_weapon.sight_vector[0][1])
-        # 弾丸格納用配列の作成
-        self.bullet_n = 0
-        self.global_bullet_x =[0]*MAX_EXIST_BULLET
-        self.global_bullet_y =[0]*MAX_EXIST_BULLET
-        self.bullet_flag =[False]*MAX_EXIST_BULLET     
-        self.spread_cof = 0
-        self.spread_bullet_vector = [(0,0) for i in range(MAX_EXIST_BULLET)]
         # 図形とヒットボックス作成
         self.view_sur = pygame.Surface((self.radius*2,self.radius*2))        
         self.hitbox_sur = pygame.Surface((self.radius*2*0.8,self.radius*2*0.8))        
-        self.__create()
-        
-    def __create(self,color:int=(100,200,100)) -> None:
+        self.rect = self.__create()[0]
+        self.hitbox_rect = self.__create()[1]        
+        # 弾丸格納用配列の作成&弾丸情報格納
+        self.bullet_n = 0
+        self.global_bullet_x =[self.__init_grobal_pos()[0]]*MAX_EXIST_BULLET
+        self.global_bullet_y =[self.__init_grobal_pos()[1]]*MAX_EXIST_BULLET
+        self.bullet_flag =[False]*MAX_EXIST_BULLET     
+        self.spread_cof = 0
+        self.spread_bullet_vector = [(0,0) for i in range(MAX_EXIST_BULLET)]
+
+    def __create(self,color:int=(100,200,100)) -> tuple[pygame.Rect,pygame.Rect]:
         """create bullet shape"""
         # 弾丸の図形作成
         self.view_sur.set_colorkey((0, 0, 0))
-        self.rect = pygame.draw.circle(self.view_sur,color,(self.radius, self.radius),radius=self.radius)
+        rect = pygame.draw.circle(self.view_sur,color,(self.radius, self.radius),radius=self.radius)
         # 弾丸のヒットボックス作成
         self.hitbox_sur.set_colorkey((0,0,0))
-        self.hitbox_rect = pygame.draw.rect(self.hitbox_sur,color,(0,0,self.radius*2*0.8,self.radius*2*0.8))
+        hitbox_rect = pygame.draw.rect(self.hitbox_sur,color,(0,0,self.radius*2*0.8,self.radius*2*0.8))
+        return rect,hitbox_rect
 
+    def __init_grobal_pos(self) -> tuple[float,float]:
+        # グローバル座標での弾丸初期位置
+        grobal_x = self.ship_weapon.rect.centerx - self.view_sur.get_rect().centerx
+        grobal_y = self.ship_weapon.rect.centery - self.view_sur.get_rect().centery
+        return grobal_x,grobal_y
+        
     def __set(self,time:int = 0):
         """set bullet array"""
         # 弾丸のセット
-        if time % self.ship_weapon.rate == 0:
+        if time % self.ship_weapon.rate == 0: #設定されたレートで弾丸を発射
             self.__update_bullet_vector()
             if self.bullet_flag[self.bullet_n] == False:
                 self.bullet_flag[self.bullet_n] = True
-                self.global_bullet_x[self.bullet_n] = self.ship_weapon.grobal_position_x + self.ship_weapon.sur.get_rect().centerx - self.view_sur.get_rect().centerx
-                self.global_bullet_y[self.bullet_n] = self.ship_weapon.grobal_position_y + self.ship_weapon.sur.get_rect().centery - self.view_sur.get_rect().centery
+                groblal_bullet_pos = self.__init_grobal_pos()
+                self.global_bullet_x[self.bullet_n] = groblal_bullet_pos[0]
+                self.global_bullet_y[self.bullet_n] = groblal_bullet_pos[1]
                 self.spread_cof = spread_bullet(self.spread)
                 self.spread_bullet_vector[self.bullet_n] = self.sight_vector.rotate(self.spread_cof)
             self.bullet_n = (self.bullet_n+1)%MAX_EXIST_BULLET
@@ -64,13 +73,16 @@ class WeaponBullet():
             if self.bullet_flag[i] == True:
                 self.global_bullet_x[i] = self.global_bullet_x[i] + self.ship_weapon.bullet_speed * self.spread_bullet_vector[i][0]*BULLET_SPEED_COF
                 self.global_bullet_y[i] = self.global_bullet_y[i] + self.ship_weapon.bullet_speed * self.spread_bullet_vector[i][1]*BULLET_SPEED_COF
+                distance_x = self.ship_weapon.bullet_speed * self.spread_bullet_vector[i][0]*BULLET_SPEED_COF
+                distance_y = self.ship_weapon.bullet_speed * self.spread_bullet_vector[i][1]*BULLET_SPEED_COF
+                self.rect.move_ip(self.view_sur,(distance_x,distance_y))
                 screen.blit(self.view_sur,(self.global_bullet_x[i],self.global_bullet_y[i]))
                 if self.global_bullet_x[i]<0 or self.global_bullet_y[i]<0 or self.global_bullet_x[i] > screen.get_rect().right or self.global_bullet_y[i] > screen.get_rect().bottom:
                     self.bullet_flag[i] = False
                     
     def __update_bullet_vector(self) -> None:
         self.sight_vector = pygame.math.Vector2(self.ship_weapon.sight_vector[1][0]-self.ship_weapon.sight_vector[0][0],self.ship_weapon.sight_vector[1][1]-self.ship_weapon.sight_vector[0][1])
-                    
+    
     def shot(self,screen,time):
         if self.ship_weapon.status['use'] == WEAPON_STAT.UNUSED:
             self.__move(screen)
